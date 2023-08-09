69c873f980dc7a560d684a0b275b8d9a51660b7c
SeongHyunKang <ruanelroem@gmail.com>
2023-08-10 03:50:17 +0900

    Update PlayerAttackController.cs

--- Assets/_____Scripts/PlayerAttackController.cs
+++ Assets/_____Scripts/PlayerAttackController.cs
@@ -3,7 +3,15 @@ using UnityEngine;
 public class PlayerAttackController : MonoBehaviour
 {
     [SerializeField] private Animator playerAnim;
-    public bool isPiercing;
+    private CharacterController controller;
+    private bool isPierceAnimationPlaying = false;
+
+    public bool isPiercing { get; private set; }
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
     public void Update()
     {
@@ -12,13 +20,25 @@ public class PlayerAttackController : MonoBehaviour
 
     public void Pierce()
     {
-        if (Input.GetMouseButton(0) && Input.GetMouseButton(1))
+        if (Input.GetMouseButton(0) && Input.GetMouseButton(1) && !isPierceAnimationPlaying)
         {
             playerAnim.SetBool("isPiercing", true);
+            StartPierceAnimation();
         }
-        else
+        else if (!Input.GetMouseButton(0) || !Input.GetMouseButton(1))
         {
             playerAnim.SetBool("isPiercing", false);
+            EndPierceAnimation();
         }
+    }
+
+    private void StartPierceAnimation()
+    {
+        isPierceAnimationPlaying = true;
+    }
+
+    private void EndPierceAnimation()
+    {
+        isPierceAnimationPlaying = false;
     }
 }
